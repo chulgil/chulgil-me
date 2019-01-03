45a18069260caee8888b5eb7afce1a3991d56528
codenavi <contact@chulgil.me>
2019-01-03 11:44:25 -0600

    create the landing page

--- nuxt-blog/nuxt.config.js
+++ nuxt-blog/nuxt.config.js
@@ -3,13 +3,16 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxt-blog',
+    title: 'CG.Lee',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
+      {
+        rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css'
+      },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
@@ -34,6 +37,15 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  plugins: [
+    {src:"~plugins/vue-particles",ssr:false}
+  ],
+  css: [
+    '@fortawesome/fontawesome-free-webfonts',
+    '@fortawesome/fontawesome-free-webfonts/css/fa-brands.css',
+    '@fortawesome/fontawesome-free-webfonts/css/fa-regular.css',
+    '@fortawesome/fontawesome-free-webfonts/css/fa-solid.css',
+  ],
 }
 
--- nuxt-blog/pages/index.vue
+++ nuxt-blog/pages/index.vue
@@ -1,65 +1,102 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxt-blog
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+    <section class="hero is-fullheight">
+      <no-ssr>
+        <vue-particles
+          color="#ddd"
+          :particleOpacity="0.7"
+          :particlesNumber="80"
+          shapeType="circle"
+          :particleSize="4"
+          linesColor="#777"
+          :linesWidth="1"
+          :lineLinked="true"
+          :lineOpacity="0.4"
+          :linesDistance="150"
+          :moveSpeed="3"
+          :hoverEffect="true"
+          hoverMode="grab"
+          :clickEffect="false"
+          clickMode="push"
+        >
+        </vue-particles>
+    </no-ssr>
+
+      <!-- Hero content: will be in the middle -->
+      <div class="hero-body">
+        
+        <div class="container has-text-centered">
+          
+        <div class=" columns is-centered has-text-centered">
+          <div class="column"></div>
+          <div class="box column is-7 is-6-desktop is-8-tablet" >
+            <div class="columns">
+              <div class="column is-one-third  hero is-dark">
+                <br>
+                <nav class="level">
+                  <div class="level-item has-text-centered">
+                    <figure class='image' style='width:140px'><img class='is-rounded' src='https://pbs.twimg.com/profile_images/1080492550366973952/Bt-4aQ-J_400x400.jpg'></figure>
+                  </div>
+                </nav>
+                <h1 class="title is-4">Chulgil.Lee</h1>
+                <h2 class="subtitle is-6">
+                  FullStack Developer
+                </h2>
+                <p></p>
+              </div>
+              <div class="column is-8 has-text-left">
+                <div class="column">
+                    <nav class="breadcrumb is-right" aria-label="breadcrumbs">
+                    <ul>
+                      <li><a href="https://twitter.com/chulgil_lee"><i class="fab fa-2x fa-twitter-square"></i></a></li>
+                      <li><a href="https://www.facebook.com/chulgil2"><i class="fab fa-2x fa-facebook-square"></i></a></li>
+                      <li><a href="https://www.instagram.com/chulgillee/"><i class="fab fa-2x fa-instagram"></i></a></li>
+                      <li><a href="https://github.com/chulgil"><i class="fab fa-2x fa-github"></i></a></li>
+                    </ul>
+                  </nav>
+                </div>
+                <br>
+                <h1 class=" has-text-centered"><img style="width:100px;" src="~/assets/qrcode_chulgil_me.png"></h1>
+                
+                <div class=" fa-ul">
+                    <div class="container" >
+                      <ul>
+                        <br>
+                        <li><i class="fa-li fas fa-phone"></i><h3 class="subtitle is-5">310-748-3824</h3></li>
+                        <li><i class="fa-li fas fa-home"></i>https://chulgil.me</li>
+                        <li><i class="fa-li fas fa-envelope"></i>contact@chulgil.me</li>
+                        <li><i class="fa-li fas fa-map-marker-alt"></i>
+                            5441 N East River Rd Chicago, IL 60656
+                        </li>
+                      </ul>
+                    </div>
+                </div>
+              </div>
+              
+            </div>
+		      </div>
+          <div class="column"></div>
+		    </div>
+        </div>
       </div>
-    </div>
-  </section>
+
+    </section>
+
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+  export default {
+ 
 
-export default {
-  components: {
-    AppLogo
-  }
-}
+  };
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+ .particles-js-canvas-el {
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
 </style>
-
